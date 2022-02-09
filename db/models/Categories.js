6346248241e8b9f5b9dc0bc4dcac7c71f997c590
a1000rhan <1000rhan@gmail.com>
2022-02-09 12:04:23 +0300

    last

--- db/models/Categories.js
+++ db/models/Categories.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+const mongooseSlugPlugin = require("mongoose-slug-plugin");
 
 const CategorySchema = new mongoose.Schema({
   title: { type: String, required: true },
   recipes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Recipe" }],
 });
-
+CategorySchema.plugin(mongooseSlugPlugin, { tmpl: "<%=title%>" });
 module.exports = mongoose.model("Category", CategorySchema);
